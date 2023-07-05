838e2d09a260c3be4002d7bab7a722df70248a73
kctsai03 <kyletsai2003@gmail.com>
2023-07-05 19:29:53 -0400

    final combining

--- src/get_clone_trees.py
+++ src/get_clone_trees.py
@@ -4,7 +4,7 @@ from collections import deque
 #input is [0, 1, 2, 3]
 #output is [0, 2], [0, 1], [1, 3]
 #[0, 1], [0, 2], [0, 3] etc.
-from check_tree import get_prufer_edges
+
 def get_clone_trees(vertices):
   output = []
   for i in vertices:
